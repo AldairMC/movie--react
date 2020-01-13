112db6dbb13d2e1590398629519cdf1ae23ce23c
AldairMC <aldairmc2023@gmail.com>
2020-01-13 16:13:14 +0000

    :construction: NavBar ready. NavBar responsive pending

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,14 +1,22 @@
+/* jshint ignore:start */
 import React from 'react';
 import './Navbar.css'
 import { NavLink } from 'react-router-dom';
-
+import logo from "./showApp.PNG"
 
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <NavLink to='/movies'> Movies </NavLink>
-            <NavLink to='/series'> Series </NavLink>
-            <NavLink to='/favorites'> Favorites </NavLink>
+            <div className="logo">
+              <NavLink to="/"> 
+                <img src={logo} alt="ShowApp" />
+              </NavLink> 
+            </div>
+            <div className="links">
+              <NavLink to='/movies'> Movies</NavLink>
+              <NavLink to='/series'>Series</NavLink>
+              <NavLink to='/favorites'>Favorites </NavLink>  
+            </div>
         </nav>
     );
 };
