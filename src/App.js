07d7a0bb73fb41a6a7969797153faf0e750eb5ec
AldairMC <aldairmc2023@gmail.com>
2020-01-12 23:45:31 -0500

    :art: Modified files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Router from './components/Router/Router'
 
 function App() {
   return (
-    <div className="App">
-  
+    <div>
+      <Router />
     </div>
   );
 }
