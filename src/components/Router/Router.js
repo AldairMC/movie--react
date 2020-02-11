0499c4e39256d360790c06d99b8a34b34b868955
AldairMC <aldairmc2023@gmail.com>
2020-02-11 17:33:23 -0500

    :construction: Get movies

--- src/components/Router/Router.js
+++ src/components/Router/Router.js
@@ -1,6 +1,6 @@
 /* jshint ignore:start */
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch} from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 //components
 import NavBar from '../NavBar/Navbar'
@@ -10,18 +10,34 @@ import Favorites from './Favorites'
 import Searcher from '../Searcher/Searcher'
 
 class Router extends Component {
+
+    componentDidMount() {
+        this.getMovie()
+    }
+
+    getMovie = () => {
+        const API = '69b64c77d37dfb52ac641752f9f81f79'
+        fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API}&language=en-US&page=1`)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (myJson) {
+                console.log(myJson.results);
+            });
+    }
+
     render() {
         return (
             <div>
                 <BrowserRouter>
                     <NavBar />
-                    <Searcher 
-                      title="Search for a movie, serie and videos"
+                    <Searcher
+                        title="Search for a movie, serie and videos"
                     />
                     <Switch>
-                        <Route exact path="/movies" component={Movies}/>
-                        <Route exact path="/series" component={Series}/>
-                        <Route exact path="/favorites" component={Favorites}/>
+                        <Route exact path="/movies" component={Movies} />
+                        <Route exact path="/series" component={Series} />
+                        <Route exact path="/favorites" component={Favorites} />
                     </Switch>
                 </BrowserRouter>
             </div>
