99198fa217c4689286b29fb652d9a3edea7b4657
AldairMC <aldairmc2023@gmail.com>
2020-02-18 14:24:37 -0500

    :sparkles: Add component Description

---
+++ src/components/Description/Description.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Description.css'
+
+export default function Description() {
+    return (
+        <div className="description">
+            <h2>Discover new movies and television shows</h2>
+        </div>
+    )
+}
