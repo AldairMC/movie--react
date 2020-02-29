1bed0f64bace20c794c29d42a73fff2682b4b5b2
AldairMC <aldairmc2023@gmail.com>
2020-02-29 09:38:04 -0500

    :art: NavBar responsive ready

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -6,18 +6,36 @@ import logo from "./showApp.PNG"
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <NavLink className="home" to="/">
-        <img src={logo} alt="ShowApp" />
-      </NavLink>
-      <div className="links">
-        <span>
-          <NavLink className="seccion" to='/movies'> Movies</NavLink>
-          <NavLink className="seccion midd" to='/series'>Series</NavLink>
-          <NavLink className="seccion" to='/favorites'>Favorites </NavLink>
-        </span>
-      </div>
-    </nav>
+
+    <div class="nave">
+      <header class="header">
+        <div class="logo">
+          <img src={logo} alt="ShowApp" />
+        </div>
+        <nav>
+          <ul>
+            <li><NavLink className="a" to='/movies'> Movies</NavLink></li>
+            <li><NavLink className="a" to='/series'>Series</NavLink></li>
+            <li><NavLink className="a" to='/favorites'>Favorites </NavLink></li>
+          </ul>
+        </nav>
+        <div class="menu-toggle">
+          <i class="fas fa-bars"></i>
+        </div>
+      </header>
+    </div>
+    // <nav className="navbar">
+    //   <NavLink className="home" to="/">
+    //     <img src={logo} alt="ShowApp" />
+    //   </NavLink>
+    //   <div className="links">
+    //     <span>
+    //       <NavLink className="seccion" to='/movies'> Movies</NavLink>
+    //       <NavLink className="seccion midd" to='/series'>Series</NavLink>
+    //       <NavLink className="seccion" to='/favorites'>Favorites </NavLink>
+    //     </span>
+    //   </div>
+    // </nav>
   );
 };
 
