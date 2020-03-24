f5d444bbbdb35a8697b11a3fb00eca415d429941
AldairMC <aldairmc2023@gmail.com>
2020-03-23 22:09:14 -0500

    :white_check_mark: Filter gender and year ready

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -3,26 +3,50 @@ import './Filter.css'
 
 export default class Filter extends Component {
 
-    year = () => {
-        let i = 1960
-        let content = `<li value="${i}">${i}</li>`
-        return document.getElementsByClassName('container_year').append(content)
+    selectedElYear = evt => {
+        let filter = evt.target.id
+        this.props.filterYear(filter)
+    }
+
+    selectedElGender = evt => {
+        let filter = evt.target.id
+        this.props.filterGender(filter)
     }
 
     render() {
+        let dataGen = this.props.gender
+        let dataYea = this.props.year
         return (
             <div className="container">
                 <div className="container_filter">
                     <span className="filter_year">
                         Year
                         <ul className="container_year">
-
+                            {
+                                Object.keys(dataYea).map(i => (
+                                    <li 
+                                        key={i}
+                                        onClick={evt => this.selectedElYear(evt)}
+                                        id={dataYea[i].id}>
+                                    {dataYea[i].name}
+                                    </li>
+                                ))
+                            }
                         </ul>
                     </span>
                     <span className="filter_gender">
                         Gender
                         <ul className="container_gender">
-
+                            {
+                                Object.keys(dataGen).map(i => (
+                                    <li 
+                                        key={i}
+                                        onClick={evt => this.selectedElGender(evt)}
+                                        id={dataGen[i].id}>
+                                    {dataGen[i].name}
+                                    </li>
+                                ))
+                            }
                         </ul>
                     </span>
                 </div>
